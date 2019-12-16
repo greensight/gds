c419d5a8247291fe9e530d76954df22f7fac9810
p1t1ch <belobeev.kirill@gmail.com>
2019-12-16 12:36:37 +0300

    remove styles import

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-import './styles';
-
 export { default as Button } from './components/Button';
 export { default as Storybook } from './components/Storybook';
 export { default as typography } from './scripts/typography';
