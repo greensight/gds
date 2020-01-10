632f2684e773d29adc62e0d1d1cf21e3617f3743
p1t1ch <belobeev.kirill@gmail.com>
2020-01-10 12:40:07 +0300

    setup layout exports

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 export { default as Button } from './components/Button';
+export { default as Layout } from './components/Layout';
+export { default as Section } from './components/Section';
+export { default as Container } from './components/Container';
 export { default as Storybook } from './components/Storybook';
 export { default as typography } from './scripts/typography';
 export { default as baseTheme } from './scripts/baseTheme';
