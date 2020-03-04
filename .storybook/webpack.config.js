15315cc1c060176bffd0bfc8be237c4097c5b6d1
p1t1ch <belobeev.kirill@gmail.com>
2020-03-04 15:33:08 +0300

    fix storybook

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,7 +1,7 @@
 const webpack = require('webpack');
 const { resolve } = require('path');
 
-module.exports = ({ config }) => {
+module.exports = ({ config, mode }) => {
     const iconsDir = resolve(__dirname, '../src/icons');
     const defaultSvgRule = config.module.rules.find(rule => rule.test.toString().includes('svg'));
     defaultSvgRule.exclude = iconsDir;
@@ -31,7 +31,17 @@ module.exports = ({ config }) => {
         }),
     ];
 
+    config.devtool = mode === 'DEVELOPMENT' && 'source-map';
     config.resolve.extensions.push('.ts', '.tsx');
+    config.resolve.alias = {
+        ...config.resolve.alias,
+        '@components': resolve(__dirname, '../src/components'),
+        '@utils': resolve(__dirname, '../src/utils'),
+        '@helpers': resolve(__dirname, '../src/helpers'),
+        '@icons': resolve(__dirname, '../src/icons'),
+        '@storybook-decorators': resolve(__dirname, './decorators'),
+    };
+    config.optimization.minimize = false;
     config.performance = false;
 
     return config;
