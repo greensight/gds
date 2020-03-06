b8f11297364eec88f2bd5ae736f1aefd68b30ba4
Maria Izvekova <izvekova@greensight.ru>
2020-03-06 17:20:14 +0300

    #56249 create Forms elements

--- src/components/Form/Form.d.ts
+++ src/components/Form/Form.d.ts
@@ -9,9 +9,37 @@ export interface IForm {
     className?: string;
 }
 
+export interface IFormGroup {
+    Tag: string;
+    /** Устанававает id FormField и FormLabel */
+    name: string;
+    /** Устанавилвает required полю. Если поле необязательно, показывает в лейбле опциональность */
+    isRequied: boolean;
+    /** Дополнительный класс */
+    className?: string;
+}
+
+export interface IFormLabel {
+    /** Визуально скрытый лейбл для скрин-ридеров */
+    hidden: boolean;
+    /** Добавляет в лейбл текст о необязательности поля */
+    isRequied: boolean;
+    /** Дополнительный класс */
+    className?: string;
+}
+
 export interface IFormField {
     /** Имя поля */
     name: string;
+    /** Дополнительный класс */
+    className?: string;
+}
+
+export interface IFormHint {
+    /** Тег обертки подсказки */
+    Tag: string;
+    /** Текст подсказки */
+    hint: string;
     /** Дополнительный класс */
     className?: string;
 }
--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react';
 import cn from 'classnames';
 import { Formik, Form as FormikForm, useField, useFormikContext } from 'formik';
-import { IForm, IFormField, IFormError } from './Form';
+import VisuallyHidden from '@components/VisuallyHidden';
+import { IForm, IFormField, IFormHint, IFormError } from './Form';
+import ErrorIcon from '../../icons/tokens/medium/crossCircle.svg';
 
 export const Form: React.FC<IForm> = ({ initialValues, validationSchema, onSubmit, children, className, ...props }) => {
     const baseClass = 'form';
@@ -56,5 +58,60 @@ export const FormField: React.FC<IFormField> = ({ name, children, className, req
         </div>
     );
 };
+export const FormHint: React.FC<IFormHint> = ({ Tag = 'span', hint, className, ...props }) => {
+    const baseClass = 'form__field-hint';
+    const classes = cn(baseClass, className);
+    return (
+        <Tag className={classes} {...props}>
+            {hint}
+        </Tag>
+    );
+};
+export const FormError: React.FC<IFormError> = ({ err, className }) => {
+    const baseClass = 'form__error';
+    const classes = cn(baseClass, className);
+
+    return (
+        <span className={classes}>
+            <ErrorIcon className={`${baseClass}-icon`} />
+            {err}
+        </span>
+    );
+};
+
+export const FormGroup: React.FC<IFormGroup> = ({
+    Tag = 'div',
+    name,
+    isRequired = true,
+    errorIsTop = true,
+    children,
+    className,
+    ...props
+}) => {
+    const baseClass = 'form-group';
+    const classes = cn(baseClass, className);
+    return (
+        <Tag className={classes} {...props}>
+            {children}
+        </Tag>
+    );
+};
+
+export const FormLabel: React.FC<IFormLabel> = ({ hidden = false, children, className, ...props }) => {
+    const baseClass = 'form__label';
+    const classes = cn(baseClass, className);
+
+    return (
+        <label className={classes} htmlFor={name} {...props}>
+            {hidden ? <VisuallyHidden>{children}</VisuallyHidden> : children}
+        </label>
+    );
+};
+
+Form.Field = FormField;
+Form.Label = FormLabel;
+Form.Group = FormGroup;
+Form.Hint = FormHint;
+Form.Error = FormError;
 
 export default Form;
