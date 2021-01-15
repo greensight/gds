3ec17441e8ddc02390fa7f1efb26a28972199580
Dmitry Kitsenko <kitsenko@greensight.ru>
2021-01-15 19:46:16 +0300

    change svg icons path

--- src/autokits/IconsAutokit/index.tsx
+++ src/autokits/IconsAutokit/index.tsx
@@ -17,7 +17,7 @@ const IconsAutokit = ({ headingLevel = 2 }: IconsAutokitProps) => {
     const iconsReq = require.context(`!!@svgr/webpack!${process.env.ICONS_DIR}`);
     const icons = iconsReq.keys().reduce((acc, name) => {
         const matchRes = name.match(/\.\/(.+)\.svg$/);
-        const fullPath = `@icons${matchRes?.[0].slice(1)}`;
+        const fullPath = `@svg${matchRes?.[0].slice(1)}`;
         const formattedName = matchRes?.[1];
         const nameParts = formattedName?.split('/');
 
