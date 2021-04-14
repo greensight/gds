8e1ae4d443a663782c1bc6f8c6a08b76655daa04
Dmitry Kitsenko <kitsenko@greensight.ru>
2021-04-14 10:15:32 +0300

    update storybook and fix ts emotion error

--- src/helpers/typography.ts
+++ src/helpers/typography.ts
@@ -7,6 +7,6 @@ import Theme from '../types/Theme';
 /**
  * `typography` helper with baseTheme. For use with inner components styling (such autokits).
  */
-const typography = (name: string) => typographyHelper(name, baseTheme as Theme);
+const typography = (name: string): CSSObject => typographyHelper(name, baseTheme as Theme);
 
 export default typography;
