9bcc66426f5e16f7b9fa02636aee9e6513bc9914
KVZhukh <zhukov@greensight.ru>
2024-05-28 17:51:51 +0300

    minor fixes

--- bin/scss/serializeFunctions.js
+++ bin/scss/serializeFunctions.js
@@ -6,7 +6,7 @@ async function serializeFunctions(config) {
         const mixinHead = '@function scale($value, $minorBool: false) {';
         const mixinVars = '$maxMinorValue: 40;';
         const mixinBody =
-            '@if ($minorBool) { $value: calc($value * $gs-minor); @if ($value > $maxMinorValue) { @return #{floor(calc($value / $gs)) * $gs}px;} @else { @return #{$value}px}} \n@else { @return #{calc($value * $gs)}px }';
+            '@if ($minorBool) { $value: calc($value * $gsMinor); @if ($value > $maxMinorValue) { @return #{floor(calc($value / $gs)) * $gs};} @else { @return #{$value}}} \n@else { @return #{calc($value * $gs)} }';
         const mixinFooter = '}';
         return [mixinHead, mixinVars, mixinBody, mixinFooter].join('\n');
     };
